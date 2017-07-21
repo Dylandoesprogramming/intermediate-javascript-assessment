0dc5fb9870e6adab34586120d620dfbdc5f657d5
Dylan Brown <dylandoesprogramming@gmail.com>
2017-07-21 10:49:30 -0500

    finished part 1

--- part1/assessment-part1.js
+++ part1/assessment-part1.js
@@ -8,7 +8,7 @@
 // Given the following nested functions:
 
 function daBears() {
-    var isaFurry = true;
+    var isFurry = true;
 
     function papaBear() {
         var porridge = "Too Hot!";
@@ -43,27 +43,27 @@ function daBears() {
 // Which function(s) access the "chair" variable and get "Too Big!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale1 = ["papaBear", "mamaBear"];
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale2 = ["goldilocks"];
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale3 = ["mamaBear"];
 
 // Which function(s) access the "sleepy" variable and get undefined
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale4 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale4 = ["daBears", "papaBear", "mamaBear", "goldilocks"];
 
 // Which function(s) access the isFurry variable and get true
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear"];
 
 
 // *************
@@ -82,7 +82,20 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // "charger" once, and invoke it twice on "mustang".
 
 // CODE HERE...
+function Vehicle() {
+    this.gasRemaining = 100;
 
+}
+Vehicle.prototype.drive = function() {
+    this.gasRemaining -= 25;
+}
+
+
+var charger = new Vehicle();
+charger.drive();
+var mustang = new Vehicle();
+mustang.drive();
+mustang.drive();
 
 
 
@@ -108,12 +121,25 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 
 // CODE HERE...
-
-
-
-// *************
-// * PROBLEM 4 *
-// *************
+String.prototype.grammarPolice = function() {
+        var newStr = "";
+        var Caps = true;
+        for (var i = 0; i < this.length; i++) {
+            if (Caps) {
+                newStr += this[i].toUpperCase();
+                Caps = false;
+            } else {
+                newStr += this[i].toLowerCase();
+            }
+            if (this[i] === " ") {
+                Caps = true;
+            }
+        }
+        return newStr;
+    }
+    // *************
+    // * PROBLEM 4 *
+    // *************
 
 // Write a function called valueType that has two parameters. Jasmine will pass
 // arguments into your function through the parameters.  Your function will need
@@ -126,7 +152,15 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // In all other cases, return "Different values"
 
 // CODE HERE...
-
+function valueType(arg1, arg2) {
+    if (typeof(arg1) === typeof(arg2) && arg1 == arg2) {
+        return "Exactly the same";
+    } else if (arg1 == arg2 && typeof(arg1) !== typeof(arg2)) {
+        return "Same value, different types";
+    } else {
+        return "Different values";
+    }
+}
 
 
 // *************
@@ -141,3 +175,9 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 var theAnswer = "Unknown";
 
 // CODE HERE...
+
+function promiseCatcher(arg) {
+    arg.then(function(response) {
+        theAnswer = response;
+    })
+}
